bfe39e20518ac462cfa3423a351b6e7874e135c7
Juan Morillo <jmorillos@uoc.edu>
2018-10-13 23:46:26 +0200

    Add PlayerCategory

---
+++ FCBPlayer/Model/PlayerCategory.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerCategory.swift
+//  FCBPlayer
+//
+//  Created by Jmorillo on 13/10/2018.
+//  Copyright © 2018 MorilloApps. All rights reserved.
+//
+
+import Foundation
+
+struct PlayerCategory {
+    let name: String
+    let imageName: String
+}
