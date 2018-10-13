979cf66e9fd28135fcb7060c077b7bebde116992
Juan Morillo <jmorillos@uoc.edu>
2018-10-13 22:52:33 +0200

    Initial Commit

---
+++ FCBPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FCBPlayer
+//
+//  Created by Jmorillo on 13/10/2018.
+//  Copyright © 2018 MorilloApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
