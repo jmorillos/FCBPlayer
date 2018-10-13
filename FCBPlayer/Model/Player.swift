f3e17c7b5761bd6dd974e501d81bd290acb7c42e
Juan Morillo <jmorillos@uoc.edu>
2018-10-14 00:05:12 +0200

    Add Struct Player

---
+++ FCBPlayer/Model/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  FCBPlayer
+//
+//  Created by Jmorillo on 13/10/2018.
+//  Copyright © 2018 MorilloApps. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    let name: String
+    let largeName: String
+    let placeOfBorn: String
+    let birthDate: String
+    let weight: String
+    let height: String
+    let information: String
+    let informationTechnical: String
+}
